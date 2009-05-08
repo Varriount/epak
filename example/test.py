9732c25976555c2ed9016e0e711e589c3463af3b
Grzegorz Adam Hankiewicz <gradha@titanium.sabren.com>
2009-05-08 22:24:39 +0200

    Implemented write test. Closes #219.

--- example/test.py
+++ example/test.py
@@ -3,12 +3,15 @@
 """Tests in python that the datafile functions work."""
 
 import epak
+import hashlib
 import os
 import os.path
 import sys
 
 WITH_PASS_EPAK = "with pass.epak"
 NO_PASS_EPAK = "no pass.epak"
+PY_WITH_PASS_EPAK = "py %s" % WITH_PASS_EPAK
+PY_NO_PASS_EPAK = "py %s" % NO_PASS_EPAK
 PASSWORD = "12358 Dummy password"
 STR1 = "00000000000000000000 This is a test string 1, don't hurt me!"
 STR2 = "This is a test string 2, don't hurt me!"
@@ -24,6 +27,30 @@ def precondition_check():
 		if not os.isfile(filename):
 			print "Missing %r" % filename
 			sys.exit(1)
+
+
+def pack_test(filename):
+	"""f(string) -> None
+
+	Writes into the specified filename the data for the test.
+	"""
+	pak = epak.open(filename, "w!")
+	assert pak, "Error creating test file"
+
+	# Test writting the string a a packfile, first uncompressed.
+	for compressed in range(2):
+		pak = epak.open_chunk(pak, compressed)
+		assert pak, "Error opening subchunk!"
+		# Repeat five times to allow for compression to kick in.
+		for times in range(5):
+			ret = epak.write(STRINGS[compressed], pak)
+			assert len(STRINGS[compressed]) == ret
+
+		pak = epak.close_chunk(pak)
+		assert pak
+
+	if epak.close(pak):
+		assert False, "Error closing packfile!"
 
 
 def read_skip_test(filename):
@@ -50,14 +77,35 @@ def read_skip_test(filename):
 	assert not closing, "Couldn't force close!"
 
 
+def get_md5(filename):
+	"""f(string) -> string
+
+	Returns the md5 hash of the specified file.
+	"""
+	md5 = hashlib.md5()
+	input = open(filename, "rb")
+	md5.update(input.read())
+	input.close()
+	return md5.hexdigest()
+
+
 def main():
-	print "Testing python read skipping code."
+	print "Testing python version"
 
 	epak.set_password(PASSWORD)
-	read_skip_test(WITH_PASS_EPAK);
+	read_skip_test(WITH_PASS_EPAK)
 
 	epak.set_password("")
-	read_skip_test(NO_PASS_EPAK);
+	read_skip_test(NO_PASS_EPAK)
+
+	epak.set_password(PASSWORD)
+	pack_test(PY_WITH_PASS_EPAK)
+
+	epak.set_password("")
+	pack_test(PY_NO_PASS_EPAK)
+
+	assert get_md5(WITH_PASS_EPAK) == get_md5(PY_WITH_PASS_EPAK)
+	assert get_md5(NO_PASS_EPAK) == get_md5(PY_NO_PASS_EPAK)
 
 if "__main__" == __name__:
 	main()
